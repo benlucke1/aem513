80385db54041bdb91b0061966c6e151a5f834ca6
Ben Lucke <benlucke@hotmail.com>
2023-06-23 20:23:42 -0500

    Add isentropic flow relation method

---
+++ matlab/isentropicRelations.m
@@ -0,0 +1,22 @@
+function [p_p0, t_t0] = isentropicRelations(M, gamma)
+%ISENTROPICRELATIONS Calculates the ratio of static to stagnation pressure
+%and temperatures of a flow given a Mach number assuming isentropic flow
+%and calorically perfect gas.
+%
+% Inputs
+%  M1 - Mach Number [Real]
+%  gamma - Ratio of specific heats [Real] (default = 1.4)
+%
+% Outputs
+%  p_p0 - Static to stagnation (total) pressure ratio (P/P_0)
+%  t_p0 - Static to stagnation (total) temperature ratio (T/T_0)
+
+if nargin < 2
+    gamma = 1.4
+end
+g = gamma
+
+p_po = (1+((g-1)/2).*M.^2).^((-1*gammma)./(gamma-1))
+t_po = (1+((g-1)/2).*M.^2).^(-1)
+end
+
